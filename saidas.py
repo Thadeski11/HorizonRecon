7193708ed612645f8c6d6b75db7bf120e6670a6f
Thadeski11 <163975171+Thadeski11@users.noreply.github.com>
2025-03-22 01:23:39 -0300

    Update saidas.py

--- saidas.py
+++ saidas.py
@@ -2,13 +2,18 @@ import json
 
 class Saidas():
 	def output_comum(tool, nome_arquivo):
-		arquivo_texto_simples = tool
 		if nome_arquivo:
-			with open(f"{nome_arquivo}", "w") as f:
-				f.write(arquivo_texto_simples)
+			if isinstance(tool, list):
+				with open(f"{nome_arquivo}", "w") as f:
+					for i in tool:
+						f.write(i)
+						f.write("\n")
+			else:		
+				with open(f"{nome_arquivo}", "w") as f:
+					f.write(tool)
 		else:
 			None
-
+			
 	def output_json(tool, nome_arquivo):
 		arquivo_json = json.dumps(tool)
 		if nome_arquivo:
