6703b60ba7240a5c8a2ad3f41f1daf900d8ab227
Thadeski11 <163975171+Thadeski11@users.noreply.github.com>
2025-04-01 16:53:27 -0300

    Update horizonrecon.py

--- horizonrecon.py
+++ horizonrecon.py
@@ -13,17 +13,18 @@ Escolha a ferramenta que vai utilizar:
 [cname] - Checar se os subdominios possuem um alias
 [whois] - Consultar informações de Whois 
 ''')
-parser.add_argument("--domain", help="Passar o domínio alvo.")
-parser.add_argument("--wordlist", help="Passar a payload de testes.")
+parser.add_argument("-d", "--domain", help="Passar o domínio alvo.")
+parser.add_argument("-w", "--wordlist", help="Passar a payload de testes.")
+parser.add_argument("-t", "--time", type=int, default=5, help="Definir tempo limite por segundo")
 parser.add_argument("--ipv", help="Digite [4] para ipv4, [6] para ipv6, [all] para ambos")
-parser.add_argument("--output", help="Salva resultados em um arquivo de texto simples")
-parser.add_argument("--json", help="Salvar resultados em um arquivo JSON")
+parser.add_argument("-o", "--output", help="Salva resultados em um arquivo de texto simples")
+parser.add_argument("-j", "--json", help="Salvar resultados em um arquivo JSON")
 args = parser.parse_args()
 
 if args.type == "cname":
 	async def run_check_cname():
 		check_cname = dns.CheckCNAME()
-		results_cname = await check_cname.Main(args.domain, args.wordlist)
+		results_cname = await check_cname.Main(args.domain, args.wordlist, args.time)
 		for i in results_cname:
 			print(i)
 		return results_cname
