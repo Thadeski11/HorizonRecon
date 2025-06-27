0b42fdc9f5b750da20a99c2b7b577b5cf9498eb1
Thadeski11 <163975171+Thadeski11@users.noreply.github.com>
2025-06-27 14:44:09 -0300

    Update horizonrecon.py

--- horizonrecon.py
+++ horizonrecon.py
@@ -3,23 +3,37 @@ import saidas
 import argparse
 import asyncio
 
-dns = dnsclass.Dnsscan
-saida = saidas.Saidas
+dns = classes.Tools
 
-parser = argparse.ArgumentParser(prog="Horizonrecon", description="Scans de Domínio")
+parser = argparse.ArgumentParser(prog="Horizonrecon", description="")
 parser.add_argument("--type", help='''
 Escolha a ferramenta que vai utilizar: 
 [sub] - Verificar subdominios ativos
 [cname] - Checar se os subdominios possuem um alias
-[whois] - Consultar informações de Whois 
 ''')
 parser.add_argument("-d", "--domain", help="Passar o domínio alvo.")
 parser.add_argument("-w", "--wordlist", help="Passar a payload de testes.")
-parser.add_argument("-t", "--time", type=int, default=5, help="Definir tempo limite por segundo")
-parser.add_argument("--ipv", help="Digite [4] para ipv4, [6] para ipv6, [all] para ambos")
+parser.add_argument("-t", "--time", type=int, default=10, help="Definir tempo limite por segundo")
 parser.add_argument("-o", "--output", help="Salva resultados em um arquivo de texto simples")
-parser.add_argument("-j", "--json", help="Salvar resultados em um arquivo JSON")
 args = parser.parse_args()
+
+def Output(tool, file_name):
+	if file_name:
+		if isinstance(tool, list):
+			with open(f"{file_name}", "w") as f:
+				for i in tool:
+					f.write(i + "\n")
+		elif isinstance(tool, dict):
+			with open(f"{file_name}", "w") as f:
+				for type, info in tool.items():
+					f.write(f"{type}:\n")
+					for host, ip in info.items():
+						f.write(f"{host}: {ip}\n")
+		else:
+			with open(f"{file_name}", "w") as f:
+				f.write(tool)
+	else:
+		None
 
 if args.type == "cname":
 	async def run_check_cname():
@@ -29,18 +43,13 @@ if args.type == "cname":
 			print(i)
 		return results_cname
 	results_cname = asyncio.run(run_check_cname())
-	saida.output_comum(results_cname, args.output)
-	saida.output_json(results_cname, args.json)
+	Output(results_cname, args.output)
 elif args.type == "sub":
 	async def run_check_subdomain():
 		subdomainbf = dns.SubdomainBF()
-		results_subdomains = await subdomainbf.Main(args.domain, args.wordlist, args.time, args.ipv)
-		print(results_subdomains)
+		results_subdomains = await subdomainbf.Main(args.domain, args.wordlist, args.time)
+		for alive in results_subdomains:	
+			print(alive)
 		return results_subdomains
 	results_subdomains = asyncio.run(run_check_subdomain())
-	saida.output_comum(results_subdomains, args.output)
-	saida.output_json(results_subdomains, args.json)
-elif args.type == "whois":
-	consulta_whois = dns.Whois(args.domain)
-	saida.output_comum(consulta_whois, args.output)
-	saida.output_json(consulta_whois, args.json)
+	Output(results_subdomains, args.output)
